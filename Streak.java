af672cbaa47cfa035ef474b1ff59457f8a039a94
Grimcheese <albychawula@gmail.com>
2021-03-19 14:47:20 +0800

    Added Date tracking functionality

--- Streak.java
+++ Streak.java
@@ -1,15 +1,34 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Streak
 {
 	private String name;
 	private String description;
 	private int streakLength;
-	
+	private LocalDate[] date;
 	
 	/* Constructor methods */ 
+	
+	/* Creates a Streak with no previous dates */
 	public Streak(String newName)
 	{
-		this.name = newName;
-		this.streakLength = 0;
+		name = newName;
+		streakLength = 0;
+		date = new LocalDate[streakLength + 1];
+	}
+	
+	/* Creates a Streak using a previous list of dates */
+	public Streak(String newName, LocalDate[] newDate)
+	{
+		name = newName;
+		streakLength = newDate.length;
+		
+		date = new LocalDate[streakLength + 1];
+		for(int i = 0; i < streakLength; i++)
+		{
+			date[i] = newDate[i];
+		}
 	}
 	
 	
@@ -28,4 +47,22 @@ public class Streak
 	{
 		return(streakLength);
 	}
+	
+	public void printDates()
+	{
+		for(int i = 0; i < date.length; i++)
+		{
+			System.out.println(date[i]);
+		}
+	}
+	
+	
+	/* Mutator methods */
+	// Appends today's date to the end of date
+	public void addToday()
+	{
+		date[date.length - 1] = LocalDate.now();
+		streakLength++;
+	}
+	
 }
